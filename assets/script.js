624e9e1b3c96c37534d06cff9fcebaabb3b161ca
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-10 12:31:23 -0800

    tried to use DOM manipulation to create 5 cards for each day of forecast with a for loop. Needs work

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
-var fetchButton = document.getElementById('fetch-button');
+var fetchButton = document.getElementById('fetchButton');
 
 function getApi() {
   // fetch request gets a list of all 
   var cityInput = document.querySelector('.cityInput').value
   var requestlatlongUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=825001e63987e7b8f9f6d2229d4bda71';
+  //Making sure weatherData is available. (May not be needed)
   var weatherData = null
+
+  //Getting access to API 
   fetch(requestlatlongUrl)
     .then(function (response) {
       return response.json();
@@ -17,8 +20,16 @@ function getApi() {
           return response.json();
         })
         .then(function (data) {
+          //looping through temperature at 12 noon 5 days in a row and getting desired information from data (data.list.length)
            for (i=2; i<data.list.length; i = i+8){
-             console.log(data.list[i].main.temp + 'F');
+            console.log(data.list[i].main.temp + 'F');
+            // Looping through the 5 cards desired(1 for each day's forecast) [Right now is creating a never ending loop]
+            // var weatherDivs = []
+            // for (i=0; i< weatherDivs; i+5){}
+            // var weatherCard = document.createElement('div');
+            // weatherCard.classList.add("weatherCard");
+            // weatherCard.textContent = [i];
+            
            }
           console.log(data)
           weatherData = data
@@ -26,6 +37,8 @@ function getApi() {
           var weather = document.getElementById('weather')
           weather.innerHTML = data.list[0].main.temp + 'F';
           
+
+          //will need to fetch the api for the cards
           //fetch()
         })
     });
