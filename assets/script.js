bdbeee3c3ede6322a29c31a89a22b694371c7439
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-09 22:11:17 -0800

    outputting correct data and set up for loop for weather for 5 days in a row

--- assets/script.js
+++ assets/script.js
@@ -4,25 +4,32 @@ function getApi() {
   // fetch request gets a list of all 
   var cityInput = document.querySelector('.cityInput').value
   var requestlatlongUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=825001e63987e7b8f9f6d2229d4bda71';
+  var weatherData = null
   fetch(requestlatlongUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data)
-      var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=825001e63987e7b8f9f6d2229d4bda71'
+      var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&units=imperial&appid=825001e63987e7b8f9f6d2229d4bda71'
       fetch(requestUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
+           for (i=2; i<data.list.length; i = i+8){
+             console.log(data.list[i].main.temp + 'F');
+           }
           console.log(data)
-
+          weatherData = data
+          console.log(weatherData)
           var weather = document.getElementById('weather')
-          weather.innerHTML = data.main.temp + 'F';
-
+          weather.innerHTML = data.list[0].main.temp + 'F';
+          
+          //fetch()
         })
     });
 };
 // event listener for clicking the fetchButton to respond to click to get API
 fetchButton.addEventListener('click', getApi);
+
