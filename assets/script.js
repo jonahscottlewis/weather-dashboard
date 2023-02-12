0c221cf765ab9f0c4f0a34e65459e01fbbb0eee7
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-12 14:09:02 -0800

    added jquery and now enter submits search for weather

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
-var fetchButton = document.getElementById('fetchButton');
+var fetchButton = document.getElementById('fetch-button');
+//Using Jquery to submit form when 'enter' key pressed
+// Wait for document to load
+$(document).ready(() => {
+  $('.info').on('submit', () => {
+
+    // prevents default behaviour
+    // Prevents event propagation
+    return false;
+  });
+});
+$('.info').keypress((e) => {
+
+  // Enter key corresponds to number 13
+  if (e.which === 13) {
+    $('.info').submit();
+  }
+});
+
 
 function getApi() {
   // fetch request gets a list of all 
@@ -23,7 +41,7 @@ function getApi() {
         })
         .then(function (data) {
           //looping through temperature at 12 noon 5 days in a row and getting desired information from data (data.list.length)
-           for (i=2; i<data.list.length; i = i+8){
+          for (i = 2; i < data.list.length; i = i + 8) {
             console.log(data.list[i].main.temp + 'F');
             //Creating empty array to be filled with current [i]
             // var weatherDivs = []
@@ -32,10 +50,10 @@ function getApi() {
             // var weatherCard = document.createElement('div');
             // weatherCard.classList.add("weatherCard");
             // weatherCard.textContent = [i]};
-            
-           }
 
-           //Making data available to the global scope? Incase needed.
+          }
+
+          //Making data available to the global scope? Incase needed.
           console.log(data)
           weatherData = data
           console.log(weatherData)
@@ -43,10 +61,10 @@ function getApi() {
           // adding the value of the current temp to weather div in HTML
           var weather = document.getElementById('weather')
           weather.innerHTML = data.list[0].main.temp + 'F';
-          
 
           //will need to fetch the api for the cards
           //fetch(requestIconUrl)
+
         })
     });
 };
